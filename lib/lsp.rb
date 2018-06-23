eaf3c6126177380d1359ca716b85880d9838deac
Rafał Łasocha <me@swistak35.com>
2018-06-23 11:18:49 +0200

    Extracted FileLanguageServer

--- lib/lsp.rb
+++ lib/lsp.rb
@@ -1,5 +1,3 @@
-require 'json'
-
 module Lsp
   TextDocumentIdentifier = Struct.new(:uri)
 
@@ -29,59 +27,6 @@ module Lsp
       raise NotImplementedError
     end
   end
-
-  class FileLanguageServer
-    def initialize(implementation, input = $stdin, output = $stdout)
-      @implementation = implementation
-      @input = input
-      @output = output
-    end
-
-    def start
-      prepare
-
-      loop do
-        headers = {}
-        loop do
-          header_line = input.readline.strip
-          if header_line.empty?
-            if headers.empty?
-              redo
-            else
-              break
-            end
-          end
-          header_name, header_value = header_line.split(":", 2)
-          headers[header_name.strip] = header_value.strip
-        end
-
-        body_raw = input.read(headers["Content-Length"].to_i)
-        body_json = JSON.parse(body_raw, symbolize_names: true)
-        implementation.request(
-          body_json.fetch(:id),
-          body_json.fetch(:method),
-          body_json.fetch(:params))
-      end
-    rescue EOFError
-    end
-
-    def prepare
-      implementation.language_server = self
-    end
-
-    def response(id, result, error)
-      output.write(build_message({
-        id: id,
-        result: result,
-        error: error,
-      }))
-    end
-
-    def build_message(hash)
-      json = hash.to_json
-      "Content-Length: #{json.size}\r\n\r\n#{json}"
-    end
-
-    attr_reader :implementation, :input, :output
-  end
 end
+
+require 'lsp/file_language_server'
