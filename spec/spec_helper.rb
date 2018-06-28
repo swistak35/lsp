42d2dffdf775273be40a8bda4b9e9d6330ff0b56
Rafał Łasocha <me@swistak35.com>
2018-06-28 10:18:07 +0200

    Add timeout for mutant

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,11 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'lsp'
+
+require 'timeout'
+
+RSpec.configure do |config|
+  config.around(:each) do |example|
+    timeout = example.metadata[:timeout] || 5
+    Timeout.timeout(timeout, &example)
+  end
+end
